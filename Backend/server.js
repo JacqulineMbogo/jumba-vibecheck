150146a6f57d634fb18a0b4f6757ba99c12592a6
JacqulineMbogo <jacqulinembogo@gmail.com>
2025-05-08 00:30:17 +0300

    Use dynamic herouku port

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,9 @@ const app = require('./app');
 mongoose
   .connect(`${process.env.DB_CONNECTION_STRING}`)
   .then(() => {
-    app.listen(5001, () => console.log('Server running on port 5001'));
+    app.listen(process.env.PORT || 5001, () =>
+      console.log('Server running on port 5001')
+    );
   })
   .catch((err) => {
     console.error('DB connection failed', err);
